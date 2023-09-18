75e2814e9803720c7528029444422f3a84470d22
Nikolas Louret <navlouret@sga.pucminas.br>
2023-09-18 00:54:45 -0300

    feat: criação de interface props

---
+++ portfolio-website/src/types/IProjectCardProps.tsx
@@ -0,0 +1,9 @@
+export interface IProjectCardProps {
+	id: number
+	name: string
+	year: number
+	description: string
+	topics: string[]
+	img: string
+	httpRepo: string
+}
---
+++ portfolio-website/src/types/IRepoProps.tsx
@@ -0,0 +1,9 @@
+export interface IRepoProps {
+	id: number
+	name: string
+	created_at: string
+	description: string
+	topics: string[]
+	contents_url: string
+	html_url: string
+}
