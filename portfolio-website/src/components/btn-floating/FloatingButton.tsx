12e42014bd16c6a2b8ce506ee0a35633f838605b
Nikolas Louret <navlouret@sga.pucminas.br>
2023-09-18 00:56:19 -0300

    feat: Adição dos bnts flutuantes

---
+++ portfolio-website/src/components/btn-floating/FloatingButton.tsx
@@ -0,0 +1,25 @@
+//* CSS
+import styles from './FloatingButton.module.css'
+
+//* Icons
+import { BsWhatsapp } from 'react-icons/bs'
+import { AiOutlineMail } from 'react-icons/ai'
+
+const FloatingButton = () => {
+	return (
+		<div className={`flex flex-column row-gap-1rem ${styles.body}`}>
+			<span className={`flex justify-center align-center ${styles.email}`}>
+				<a href='mailto:nikolasalouret@gmail.com' target='_blank'>
+					<AiOutlineMail size='1.7rem' fill='#fff' />
+				</a>
+			</span>
+			<span className={`flex justify-center align-center ${styles.whatsapp}`}>
+				<a href='https://wa.me//5531999532003' target='_blank'>
+					<BsWhatsapp size='1.7rem' fill='#fff' />
+				</a>
+			</span>
+		</div>
+	)
+}
+
+export default FloatingButton
