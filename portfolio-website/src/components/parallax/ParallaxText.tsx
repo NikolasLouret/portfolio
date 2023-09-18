b3c4514c9262f3360bf64266eb284667920074be
Nikolas Louret <navlouret@sga.pucminas.br>
2023-09-18 00:52:55 -0300

    feat: Adição de comp de parallax

---
+++ portfolio-website/src/components/parallax/ParallaxText.tsx
@@ -0,0 +1,58 @@
+import styles from "./ParallaxText.module.css";
+
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame,
+} from "framer-motion";
+
+import { wrap } from "@motionone/utils";
+
+interface ParallaxProps {
+  children: string | JSX.Element;
+  baseVelocity: number;
+}
+
+const ParallaxText = ({ children, baseVelocity = 1 }: ParallaxProps) => {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 30,
+    stiffness: 200,
+  });
+
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false,
+  });
+
+  const x = useTransform(baseX, (v) => `${wrap(-20, -70, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+
+    if (velocityFactor.get() > 0) moveBy += moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+  });
+
+  return (
+    <div className={styles.parallax}>
+      <motion.div className={styles.scroller} style={{ x }}>
+        <span>{children}</span>
+        <span>{children}</span>
+        <span>{children}</span>
+        <span>{children}</span>
+      </motion.div>
+    </div>
+  );
+};
+
+export default ParallaxText;
