37d345384b8c21266d039522ceee78d1c1df2d8b
Nikolas Louret <navlouret@sga.pucminas.br>
2023-09-19 13:36:59 -0300

    refactor: Refatoração do "sobre mim"

--- portfolio-website/src/pages/sections/about-me/AboutMe.tsx
+++ portfolio-website/src/pages/sections/about-me/AboutMe.tsx
@@ -7,19 +7,38 @@ const AboutMe = (prop: { id: string }) => {
 		<section id={prop.id} className={`flex flex-column ${styles.body}`}>
 			<FadeInSection direction='left'>
 				<div className={styles.title}>
-					<span className={styles.subtitle}>Algumas palavras</span>
+					<span className={styles.subtitle}>Um pouco</span>
 					<h2 className={styles.about_me}>SOBRE MIM</h2>
 				</div>
 			</FadeInSection>
 
-			<FadeInSection direction='up'>
-				<p className={styles.description}>
-					Sou estudante de Engenharia de Software pela PUC Minas e moro em Contagem/MG. Possuo habilidade em
-					desenvolvimento Full Stack e bom raciocínio lógico. Portanto, meu objetivo é atuar em projetos de
-					desenvolvimento Full Stack a fim de contribuir com o meu crescimento profissional e agregar valor
-					para a empresa.
-				</p>
-			</FadeInSection>
+			<div className={`flex flex-column ${styles.description}`}>
+				<FadeInSection direction='up'>
+					<p>
+						Sou estudante de Engenharia de Software na PUC Minas, atualmente residindo em Contagem, MG.
+						Minha paixão pela programação me levou a desenvolver habilidades sólidas em desenvolvimento Full
+						Stack e a cultivar pensamento lógico. Meu objetivo profissional é mergulhar em projetos de
+						desenvolvimento Full Stack/Front End, onde posso continuar aprimorando minhas capacidades e, ao
+						mesmo tempo, fazer uma contribuição valiosa para a equipe e a empresa.
+					</p>
+				</FadeInSection>
+				<FadeInSection direction='up'>
+					<p>
+						Minha jornada até aqui tem sido repleta de experiências enriquecedoras. Tenho experiência
+						prática com várias tecnologias, incluindo as linguagens Java e JavaScript. Participei do
+						desenvolvimento de projetos Full Stack, utilizando ferramentas como ReactJs, NodeJs, Express,
+						Sequelize, Mongoose, CSS e HTML. Além disso, trabalhei com modelagem de bancos de dados tanto
+						relacionais, usando MySQL e MS SQLServer, quanto não-relacionais, na nuvem, com MongoDB Atlas.
+					</p>
+				</FadeInSection>
+				<FadeInSection direction='up'>
+					<p>
+						Tenho grandes aspirações para o futuro e desejo expandir meus conhecimentos para o campo das
+						aplicações móveis. Estou ansioso para explorar o desenvolvimento mobile com ReactJs Native e
+						Flutter, a fim de ampliar ainda mais meu conjunto de habilidades.
+					</p>
+				</FadeInSection>
+			</div>
 		</section>
 	)
 }
