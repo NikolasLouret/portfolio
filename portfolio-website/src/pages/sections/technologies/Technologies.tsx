bd1ebbc2410a07fef93b8198767c018c4114e037
Nikolas Louret <navlouret@sga.pucminas.br>
2023-09-19 00:30:38 -0300

    feat: Adição de titulo e correção da animação das tecnologias

--- portfolio-website/src/pages/sections/technologies/Technologies.tsx
+++ portfolio-website/src/pages/sections/technologies/Technologies.tsx
@@ -3,97 +3,98 @@ import styles from './Technologies.module.css'
 
 const Technologies = () => {
 	return (
-		<div className={`flex justify_between ${styles.tech}`}>
-			<ul className={`flex justify_between ${styles.list_tech}`}>
-				<li>
-					<img src='/icons/git.svg' alt='Git' />
-				</li>
-				<li>
-					<img src='/icons/css3.svg' alt='CSS3' />
-				</li>
-				<li>
-					<img src='/icons/javascript.svg' alt='JavaScript' />
-				</li>
-				<li>
-					ne
-					<img src='/icons/html.svg' alt='HTML5' />
-				</li>
-				<li>
-					<img src='/icons/mongodb.svg' alt='MongoDB' />
-				</li>
-				<li>
-					<img src='/icons/nodejs.svg' alt='NodeJs' />
-				</li>
-				<li>
-					<img src='/icons/react-icon.svg' alt='ReactJs' />
-				</li>
-				<li>
-					<img src='/icons/java.svg' alt='Java' />
-				</li>
-				<li>
-					<img src='/icons/intellij.svg' alt='IntelliJ IDE' />
-				</li>
-			</ul>
-			<ul className={`flex justify_between ${styles.list_tech}`}>
-				<li>
-					<img src='/icons/git.svg' alt='Git' />
-				</li>
-				<li>
-					<img src='/icons/css3.svg' alt='CSS3' />
-				</li>
-				<li>
-					<img src='/icons/javascript.svg' alt='JavaScript' />
-				</li>
-				<li>
-					ne
-					<img src='/icons/html.svg' alt='HTML5' />
-				</li>
-				<li>
-					<img src='/icons/mongodb.svg' alt='MongoDB' />
-				</li>
-				<li>
-					<img src='/icons/nodejs.svg' alt='NodeJs' />
-				</li>
-				<li>
-					<img src='/icons/react-icon.svg' alt='ReactJs' />
-				</li>
-				<li>
-					<img src='/icons/java.svg' alt='Java' />
-				</li>
-				<li>
-					<img src='/icons/intellij.svg' alt='IntelliJ IDE' />
-				</li>
-			</ul>
-			<ul className={`flex justify_between ${styles.list_tech}`}>
-				<li>
-					<img src='/icons/git.svg' alt='Git' />
-				</li>
-				<li>
-					<img src='/icons/css3.svg' alt='CSS3' />
-				</li>
-				<li>
-					<img src='/icons/javascript.svg' alt='JavaScript' />
-				</li>
-				<li>
-					ne
-					<img src='/icons/html.svg' alt='HTML5' />
-				</li>
-				<li>
-					<img src='/icons/mongodb.svg' alt='MongoDB' />
-				</li>
-				<li>
-					<img src='/icons/nodejs.svg' alt='NodeJs' />
-				</li>
-				<li>
-					<img src='/icons/react-icon.svg' alt='ReactJs' />
-				</li>
-				<li>
-					<img src='/icons/java.svg' alt='Java' />
-				</li>
-				<li>
-					<img src='/icons/intellij.svg' alt='IntelliJ IDE' />
-				</li>
-			</ul>
+		<div className={styles.tech}>
+			<h3 className={styles.tech_title}>Tecnologias</h3>
+
+			<div className={`flex justify_between ${styles.technologies}`}>
+				<ul className={`flex justify_between ${styles.list_tech}`}>
+					<li>
+						<img src='/icons/git.svg' alt='Git' />
+					</li>
+					<li>
+						<img src='/icons/css3.svg' alt='CSS3' />
+					</li>
+					<li>
+						<img src='/icons/javascript.svg' alt='JavaScript' />
+					</li>
+					<li>
+						<img src='/icons/html.svg' alt='HTML5' />
+					</li>
+					<li>
+						<img src='/icons/mongodb.svg' alt='MongoDB' />
+					</li>
+					<li>
+						<img src='/icons/nodejs.svg' alt='NodeJs' />
+					</li>
+					<li>
+						<img src='/icons/react-icon.svg' alt='ReactJs' />
+					</li>
+					<li>
+						<img src='/icons/java.svg' alt='Java' />
+					</li>
+					<li>
+						<img src='/icons/intellij.svg' alt='IntelliJ IDE' />
+					</li>
+				</ul>
+				<ul className={`flex justify_between ${styles.list_tech}`}>
+					<li>
+						<img src='/icons/git.svg' alt='Git' />
+					</li>
+					<li>
+						<img src='/icons/css3.svg' alt='CSS3' />
+					</li>
+					<li>
+						<img src='/icons/javascript.svg' alt='JavaScript' />
+					</li>
+					<li>
+						<img src='/icons/html.svg' alt='HTML5' />
+					</li>
+					<li>
+						<img src='/icons/mongodb.svg' alt='MongoDB' />
+					</li>
+					<li>
+						<img src='/icons/nodejs.svg' alt='NodeJs' />
+					</li>
+					<li>
+						<img src='/icons/react-icon.svg' alt='ReactJs' />
+					</li>
+					<li>
+						<img src='/icons/java.svg' alt='Java' />
+					</li>
+					<li>
+						<img src='/icons/intellij.svg' alt='IntelliJ IDE' />
+					</li>
+				</ul>
+				<ul className={`flex justify_between ${styles.list_tech}`}>
+					<li>
+						<img src='/icons/git.svg' alt='Git' />
+					</li>
+					<li>
+						<img src='/icons/css3.svg' alt='CSS3' />
+					</li>
+					<li>
+						<img src='/icons/javascript.svg' alt='JavaScript' />
+					</li>
+					<li>
+						<img src='/icons/html.svg' alt='HTML5' />
+					</li>
+					<li>
+						<img src='/icons/mongodb.svg' alt='MongoDB' />
+					</li>
+					<li>
+						<img src='/icons/nodejs.svg' alt='NodeJs' />
+					</li>
+					<li>
+						<img src='/icons/react-icon.svg' alt='ReactJs' />
+					</li>
+					<li>
+						<img src='/icons/java.svg' alt='Java' />
+					</li>
+					<li>
+						<img src='/icons/intellij.svg' alt='IntelliJ IDE' />
+					</li>
+				</ul>
+			</div>
 		</div>
 	)
 }
